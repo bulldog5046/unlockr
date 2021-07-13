4e769aef2e3fbef2049f9c98f0cfa2240649539c
bulldog5046 <bulldog5046@gmail.com>
2021-07-13 09:25:15 +0100

    Update unlockr.sh
    updated for 7.0.0

--- unlockr.sh
+++ unlockr.sh
@@ -8,12 +8,12 @@ fi
 
 echo "Backing up original files to ~/backup/"
 mkdir ~/backup
-cp /var/sf/lib/perl/5.10.1/SF/SmartAgentManager.pm ~/backup/
-cp /var/sf/lib/perl/5.10.1/SF/License/SmartLicense.pm ~/backup/
+cp /var/sf/lib/perl/*.*/SF/SmartAgentManager.pm ~/backup/
+cp /var/sf/lib/perl/*.*/SF/License/SmartLicense.pm ~/backup/
 
 echo "Applying configuration changes.."
-sed -i 's/$dbg_str = "getExportFeature start";/return 1;\n    $dbg_str = "getExportFeature start";/' /var/sf/lib/perl/5.10.1/SF/SmartAgentManager.pm
-sed -i 's/sub getRegistrationStatus {/sub getRegistrationStatus {\n    return { status => getFakeStatus( $AUTH_STATE_AUTHORIZED, $REG_STATE_REGISTERED ) };/' /var/sf/lib/perl/5.10.1/SF/License/SmartLicense.pm
+sed -i 's/$dbg_str = "getExportFeature start";/return 1;\n    $dbg_str = "getExportFeature start";/' /var/sf/lib/perl/*.*/SF/SmartAgentManager.pm
+sed -i 's/sub getRegistrationStatus {/sub getRegistrationStatus {\n    return { status => getFakeStatus( $AUTH_STATE_AUTHORIZED, $REG_STATE_REGISTERED ) };/' /var/sf/lib/perl/*.*/SF/License/SmartLicense.pm
 
 echo "Changes Applied. The host will now reboot."
 echo "Verify smart license status in the admin console after reboot."
